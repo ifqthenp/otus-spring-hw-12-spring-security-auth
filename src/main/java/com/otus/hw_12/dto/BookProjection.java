60ec7a8b6341c22357a98f25721d62e9e797a546
Andrei Bogomja <bogomja@gmail.com>
2019-04-08 23:31:59 +0100

    Updates javadoc

--- src/main/java/com/otus/hw_12/dto/BookProjection.java
+++ src/main/java/com/otus/hw_12/dto/BookProjection.java
@@ -1,13 +1,13 @@
 package com.otus.hw_12.dto;
 
-import com.otus.hw_12.controllers.LibraryController;
+import com.otus.hw_12.controllers.BookSearchController;
 import org.springframework.ui.Model;
 
 /**
  * This projection of book is to be used in search result provided by
  * {@code BookService} and initiated by user in book search form.
  *
- * @see LibraryController#fullSearch(Model, String, String, String)
+ * @see BookSearchController#fullSearch(Model, String, String, String)
  */
 public interface BookProjection {
 
