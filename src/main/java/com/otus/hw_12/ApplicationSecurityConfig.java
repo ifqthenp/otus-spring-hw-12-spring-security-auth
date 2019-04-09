369683f63fa873509715359668940ef25cbf999a
Andrei Bogomja <bogomja@gmail.com>
2019-04-09 12:23:02 +0100

    Injects library user details service

--- src/main/java/com/otus/hw_12/ApplicationSecurityConfig.java
+++ src/main/java/com/otus/hw_12/ApplicationSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.otus.hw_12;
 
+import com.otus.hw_12.auth.LibraryUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static String[] mvcPatterns = new String[]{"/", "/home"};
+
+    private final LibraryUserDetailsService userDetailsService;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
