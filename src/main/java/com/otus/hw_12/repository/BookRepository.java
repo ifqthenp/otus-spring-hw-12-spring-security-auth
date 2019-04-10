70158d495474a6850e35b2f1a05ac4a4fca97ab8
Andrei Bogomja <bogomja@gmail.com>
2019-04-10 13:14:20 +0100

    Use Postres syntax in native queries

--- src/main/java/com/otus/hw_12/repository/BookRepository.java
+++ src/main/java/com/otus/hw_12/repository/BookRepository.java
@@ -24,9 +24,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Iterable<Book> findByTitleContainingIgnoreCase(String title);
 
     @Query(value = "SELECT" +
-        "  books.title                                                                              AS title,\n" +
-        "  GROUP_CONCAT(DISTINCT CONCAT(authors.first_name, ' ', authors.last_name) SEPARATOR ', ') AS authors,\n" +
-        "  GROUP_CONCAT(DISTINCT genres.genre SEPARATOR ', ')                                       AS genres\n" +
+        "  books.title                                                                       AS title,\n" +
+        "  string_agg(DISTINCT concat(authors.first_name || ' ' || authors.last_name), ', ') AS authors,\n" +
+        "  string_agg(DISTINCT genres.genre, ', ')                                           AS genres\n" +
         "FROM books,\n" +
         "     authors,\n" +
         "     genres,\n" +
@@ -36,14 +36,14 @@ public interface BookRepository extends JpaRepository<Book, Long> {
         "  AND authors.id = book_author.author_id\n" +
         "  AND books.id = book_genre.book_id\n" +
         "  AND genres.id = book_genre.genre_id\n" +
-        "  AND LOWER(books.title) LIKE LOWER(CONCAT('%', TRIM(:title), '%'))\n" +
+        "  AND lower(books.title) LIKE lower(concat('%', trim(:title), '%'))\n" +
         "GROUP BY Title", nativeQuery = true)
     List<BookProjection> findBooksByTitle(@Param("title") String title);
 
     @Query(value = "SELECT" +
-        "  books.title                                                                              AS title,\n" +
-        "  GROUP_CONCAT(DISTINCT CONCAT(authors.first_name, ' ', authors.last_name) SEPARATOR ', ') AS authors,\n" +
-        "  GROUP_CONCAT(DISTINCT genres.genre SEPARATOR ', ')                                       AS genres\n" +
+        "  books.title                                                                       AS title,\n" +
+        "  string_agg(DISTINCT concat(authors.first_name || ' ' || authors.last_name), ', ') AS authors,\n" +
+        "  string_agg(DISTINCT genres.genre, ', ')                                           AS genres\n" +
         "FROM books,\n" +
         "     authors,\n" +
         "     genres,\n" +
@@ -53,9 +53,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
         "  AND authors.id = book_author.author_id\n" +
         "  AND books.id = book_genre.book_id\n" +
         "  AND genres.id = book_genre.genre_id\n" +
-        "  AND LOWER(books.title) LIKE LOWER(CONCAT('%', TRIM(:title), '%'))\n" +
-        "  AND LOWER(CONCAT(TRIM(authors.first_name), ' ', TRIM(authors.last_name))) LIKE LOWER(CONCAT('%', TRIM(:author), '%'))\n" +
-        "  AND LOWER(genres.genre) LIKE LOWER(CONCAT('%', TRIM(:genre), '%'))\n" +
+        "  AND lower(books.title) LIKE lower(concat('%', trim(:title), '%'))\n" +
+        "  AND lower(concat(trim(authors.first_name), ' ', trim(authors.last_name))) LIKE lower(concat('%', trim(:author), '%'))\n" +
+        "  AND lower(genres.genre) LIKE lower(concat('%', trim(:genre), '%'))\n" +
         "GROUP BY Title", nativeQuery = true)
     List<BookProjection> findBooksByRequestParameters(@Param("title") String title,
                                                       @Param("author") String author,
