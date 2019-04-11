8b1a9fadf5b7a466d7f777a658d04c660b3b379e
Andrei Bogomja <bogomja@gmail.com>
2019-04-11 12:37:17 +0100

    Return a set of authorities for a given user from getAuthorities() method

--- src/main/java/com/otus/hw_12/auth/LibraryUserPrincipal.java
+++ src/main/java/com/otus/hw_12/auth/LibraryUserPrincipal.java
@@ -5,17 +5,23 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 @RequiredArgsConstructor
 public class LibraryUserPrincipal implements UserDetails {
 
     private final User user;
+    private final List<AuthGroup> authGroups;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+        if (null == authGroups) {
+            return Collections.emptySet();
+        }
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        authGroups.forEach(group ->
+            grantedAuthorities.add(new SimpleGrantedAuthority(group.getAuthGroup())));
+        return grantedAuthorities;
     }
 
     @Override
